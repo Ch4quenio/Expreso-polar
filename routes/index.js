b211569351fdbf7bc493b1f726b44680ad5401ec
Lisandro <lisandrogomez732@gmail.com>
2021-07-12 16:08:40 -0300

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Ositos polares' });
+});
+
+router.get('/contacto',(req,res) => {
+res.render("pages/contacto");
+})
+router.get('/nosotros',(req,res) => {
+res.render("pages/nosotros");
+})
+
+
+
+module.exports = router;
